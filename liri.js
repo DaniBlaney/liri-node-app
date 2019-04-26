b951540e3d876094bdf4a68b205169bc06ec4637
Danielle Blaney <daniblaney@gmail.com>
2019-04-25 22:48:29 -0400

    created variables for input

--- liri.js
+++ liri.js
@@ -1,5 +1,13 @@
-require("dotenv").config();
+var dotenv = require("dotenv").config();
 
 var keys = require("./keys.js");
+//var Spotify = require('node-spotify-api')
+//var request = require('request')
+//var fs = require('fs')
+
 
 var spotify = new Spotify(keys.spotify);
+
+//variable for input
+var command = process.argv[2];
+var input = process.argv[3];
