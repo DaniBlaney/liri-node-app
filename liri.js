5144f94ec894ed71bc80eedbfd8bdd8003c1403b
Danielle Blaney <daniblaney@gmail.com>
2019-04-25 23:03:20 -0400

    spotifySong function

--- liri.js
+++ liri.js
@@ -11,3 +11,25 @@ var spotify = new Spotify(keys.spotify);
 //variable for input
 var command = process.argv[2];
 var input = process.argv[3];
+
+// spotify this song
+function spotifySong(musicSearch) {
+
+  if (musicSearch === undefined || null) {
+    musicSearch = "";
+  }
+
+  spotify.search({type: 'track', query: musicSearch}, function (err, data)
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    else {
+      for (i = 0; i < data.tracks.items.length && i < 5; i++){
+
+        var musicQuery = data.tracks.items[i];
+          console.log("Artist: " + musicQuery.artists[0].name + "\nSong Name: "
+          + musicQuery.name + "\nAlbum Name: " + musicQuery.album.name);
+      };
+    };
+  });
+}
