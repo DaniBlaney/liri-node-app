9bf7d041d03a90e0e5c95bba3cea8613a696f819
Danielle Blaney <daniblaney@gmail.com>
2019-04-25 23:30:26 -0400

    bands in town concert this

--- liri.js
+++ liri.js
@@ -4,7 +4,11 @@ var Spotify = require('node-spotify-api')
 var request = require('request')
 var fs = require('fs')
 
+//moment
+var moment = require('moment');
+moment().format();
 
+//get spotify keys
 var spotify = new Spotify(keys.spotify);
 
 //variable for input
@@ -29,8 +33,9 @@ function spotifySong(musicSearch) {
           console.log("Artist: " + musicQuery.artists[0].name + "\nSong Name: "
           + musicQuery.name + "\nAlbum Name: " + musicQuery.album.name);
       };
-    };
-  });
+    });
+
+  };
 spotifySong();
 
 //ombd movie-this
@@ -40,10 +45,10 @@ function movieThis (movieQuery) {
     movieQuery = "";
   }
 
-  var queryURL = "http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&apikey=trilogy";
-  console.log(queryURL)
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&apikey=trilogy";
+  console.log(queryUrl)
 
-  request(queryURL, function(error, response, body){
+  request(queryUrl, function(error, response, body){
     if (!error && response.statusCode === 200){
       var movieData = JSON.parse(body);
         console.log("Movie Title: " + movieData.Title + "\nYear: " + movieData.released
@@ -52,5 +57,26 @@ function movieThis (movieQuery) {
         movieData.Language + "\nPlot: " + movieData.Plot + "\nActors: " + movieData.Actors)
     };
   });
+
 };
 movieThis();
+
+// Bands in Town concertThis
+function concertThis(bandQuery){
+
+  var queryUrl = "https://rest.bandsintown.com/artists/" + bandQuery + "/events?app_id=codingbootcamp#";
+  console.log(queryUrl);
+
+  request(queryUrl, function (error, response, body){
+    if (!error && response.statusCode ===200){
+
+      var concertData = JSON.parse(body);
+      console.log(concertData);
+      var concertDateTime = concertData[0].datatime;
+      var momentDateTime = moment().format('L');
+      console.log("Venue Name: " + concertData[0].venue.name + "\nVenue Location: " + concertData[0].venue.city +
+      "," + concertData[0].venue.country + "\nDate of the Event: " + momentDateTime);
+    };
+  });
+
+};
